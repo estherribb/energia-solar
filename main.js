5821884ba2f626d18c4ab337866ec8a843e1160b
estherribb <147617380+estherribb@users.noreply.github.com>
2024-11-01 08:23:12 -0300

    .

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ let cartao = document.createElement('article')
 cartao.classList = 'cartao'
 
 cartao.innerHTML =`<div class="cartao--conteudo">
-            <div class="cartão--pergunta">É possível gerar energia para
+            <div class="cartao--pergunta">É possível gerar energia para
                 outras residências com apenas
                 uma usina solar?</div>
-                <div class="cartão--reposta">A “geração compartilhada” neste contexto significa que duas ou mais residências, apartamentos, empresas ou propriedades em geral podem se beneficiar da mesma energia solar gerada por uma única unidade geradora</div>
+                <div class="cartao--reposta">A “geração compartilhada” neste contexto significa que duas ou mais residências, apartamentos, empresas ou propriedades em geral podem se beneficiar da mesma energia solar gerada por uma única unidade geradora</div>
             </div>`
 
             conteudo.appendChild(cartao)
