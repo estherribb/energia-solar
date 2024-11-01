ab3831b8dadaa9686a327c860dc39bcc7da772b2
estherribb <147617380+estherribb@users.noreply.github.com>
2024-11-01 08:39:04 -0300

    .

--- pergunta.js
+++ pergunta.js
@@ -1,6 +1,20 @@
 criarCartao(
-    "É possível gerar energia para
-                outras residências com apenas
-                uma usina solar?",
-                "A “geração compartilhada” neste contexto significa que duas ou mais residências, apartamentos, empresas ou propriedades em geral podem se beneficiar da mesma energia solar gerada por uma única unidade geradora"
+    "É possível gerar energia para outras residências com apenas uma usina solar?",
+    "A “geração compartilhada” neste contexto significa que duas ou mais residências, apartamentos, empresas ou propriedades em geral podem se beneficiar da mesma energia solar gerada por uma única unidade geradora"
+)
+criarCartao(
+    "quando a energia solar foi criada?",
+    "A energia solar, próxima ao que conhecemos hoje, surgiu em 1954"
+)
+criarCartao(
+    "como é feito um painel solar?",
+    "Os painéis são basicamente formados por células feitas de materiais semicondutores como o silício, chamadas de células fotovoltaicas"
+)
+criarCartao(
+    "",
+    ""
+)
+criarCartao(
+    "",
+    ""
 )
