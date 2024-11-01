a55e9f4d7a77e1b496dc932fe0dd97e9a8e949f5
estherribb <147617380+estherribb@users.noreply.github.com>
2024-11-01 08:33:54 -0300

    .

--- pergunta.js
+++ pergunta.js
@@ -1,3 +1,6 @@
 criarCartao(
-    ""
+    "É possível gerar energia para
+                outras residências com apenas
+                uma usina solar?",
+                "A “geração compartilhada” neste contexto significa que duas ou mais residências, apartamentos, empresas ou propriedades em geral podem se beneficiar da mesma energia solar gerada por uma única unidade geradora"
 )
